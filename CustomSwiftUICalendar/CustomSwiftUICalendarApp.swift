47933a3c50d3744116320b42bc5626923a99772c
maarcq <marcellerq27@gmail.com>
2025-03-02 19:55:57 -0300

    Initial Commit

---
+++ CustomSwiftUICalendar/CustomSwiftUICalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSwiftUICalendarApp.swift
+//  CustomSwiftUICalendar
+//
+//  Created by Marcelle Ribeiro Queiroz on 02/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomSwiftUICalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
